ca55e84e145be9d864d3d54d80c9d7f032108ba8
dhana-racheal <dhanabaghyamr@gmail.com>
2024-08-22 01:21:32 +0530

    Excel Data imported to List

--- src/main/java/com/Interview_Assessment/common/ExcelUtils.java
+++ src/main/java/com/Interview_Assessment/common/ExcelUtils.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -39,6 +44,42 @@ public class ExcelUtils {
 		}
 
 		return null;
+
+	}
+	
+	public static List<String> readAllExcelData(String filePath,String sheetName) throws InterruptedException, IOException{
+File f = new File(filePath);
+		
+		FileInputStream fis;
+		Workbook wb = null;
+		List<String> arrList = null;
+		
+		try {
+			fis = new FileInputStream(f);
+			wb = new XSSFWorkbook(fis);
+			Sheet sheet = wb.getSheet(sheetName);
+		    int rowcount = sheet.getLastRowNum()- sheet.getFirstRowNum();
+		    for(int i=1; i<rowcount+1; i++){
+		        Row row = sheet.getRow(i);
+		        arrList = new ArrayList<>();
+		        for(int j=0; j<row.getLastCellNum(); j++){
+		            Cell cell = row.getCell(j);
+		            arrList.add(cell.getStringCellValue());
+		            }
+//		        Iterator<String> itr = arrName.iterator();
+//		        while(itr.hasNext()){
+//		            System.out.println("arrayList values: "+itr.next());
+//		        }
+		        }
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return arrList;
+	    
 
 	}
 
--- src/main/java/com/Interview_Assessment/pages/HomePage.java
+++ src/main/java/com/Interview_Assessment/pages/HomePage.java
@@ -1,5 +1,6 @@
 package com.Interview_Assessment.pages;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -73,26 +74,39 @@ public class HomePage extends BasePage{
 
 	public void getProfileName() {
 		profile_list = new ArrayList<>();
-		title_list = new ArrayList<>();
-		loc_list = new ArrayList<>();
-		category_list = new ArrayList<>();
-		for (int i = 0; i < 2; i++) {
-			String prof_name = ExcelUtils.readExcelData(".\\src\\main\\resources\\kynhood.xlsx", "klips_data", i, 0);
+		
 
-			for (int j = 0; j < profile_list.size(); j++) {
-				if (prof_name.equals(profile_list.get(j))) {
-					Assert.assertTrue(profile_list.contains(prof_name));
-				}
+			try {
+				profile_list = ExcelUtils.readAllExcelData(".\\src\\main\\resources\\kynhood.xlsx", "klips_data");
+			} catch (InterruptedException | IOException e) {
+				e.printStackTrace();
 			}
-			profile_list.add(getText(profile_name_list.get(i)));
-			title_list.add(getText(content_title_list.get(i)));
-			loc_list.add(getText(location_list.get(i)));
-			category_list.add(getText(category_name_list.get(i)));
+			for (int i = 0; i < 3; i++) {
+				if(profile_list.contains(getText(profile_name_list.get(i)))) {
+					Assert.assertTrue(true);
+				System.out.println("##############"+getText(profile_name_list.get(i)));
+				}
+				else if(profile_list.contains(getText(content_title_list.get(i)))) {
+					Assert.assertTrue(true);
+					System.out.println("##############"+getText(content_title_list.get(i)));
+				}
+					
+				else if(profile_list.contains(getText(location_list.get(i)))) {
+					System.out.println("##############"+getText(location_list.get(i)));
+					Assert.assertTrue(true);
+				}
+					
+				else if(profile_list.contains(getText(category_name_list.get(i)))) {
+					Assert.assertTrue(true);
+					System.out.println("##############"+getText(category_name_list.get(i)));
+				}
+					
+			}
+			System.out.println(profile_list);
+			
+			
 		}
-		System.out.println(profile_list);
-		System.out.println(title_list);
-		System.out.println(loc_list);
-		System.out.println(category_list);
-	}
+		
+	
 
 }
